0b98cc0df8b68a087965ba3ee334273a81d4776b
Maciej Dzik <dzikdrums@gmail.com>
2020-01-31 09:22:48 +0100

    add add/remove functions to tasks

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 const express = require('express');
-const path = require('path');
 const socket = require('socket.io');
 const app = express();
 
-const tasks = ['buty', 'zakupy'];
-
-app.use(express.static(path.join(__dirname, './client/public')));
-app.use(express.static(path.join(__dirname, './client/src')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/src/index.js'));
-});
+const tasks = [];
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
@@ -20,19 +12,19 @@ const io = socket(server);
 
 io.on('connection', (socket) => {
   console.log('new user has connected');
-  socket.emit('udpateData', tasks)
+  socket.emit('updateData', tasks)
 
   socket.on('addTask', (task) => {
-    console.log('addtask');
     tasks.push(task);
-    socket.broadcast.emit('addTask', task);
+    socket.broadcast.emit('addTask', task, 'emitted');
   });
 
-  socket.on('removeTask', (index) => {
-    console.log('removeTask');
-    tasks.splice(index, 1);
-    socket.broadcast.emit('removeTask', index);
+  socket.on('removeTask', (id) => {
+    tasks.forEach((item, index) => {
+      if( item.id === id) {
+        tasks.splice(index, 1);
+      };
+    });
+    socket.broadcast.emit('removeTask', id, 'emitted');
   });
-
-
 });
